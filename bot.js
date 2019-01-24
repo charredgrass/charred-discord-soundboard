7ccb27aee38cf6c78e632b1aca92a53a3d68530d
Maxwell Chow <maxchowla@gmail.com>
2019-01-24 00:06:33 -0800

    working proto

--- bot.js
+++ bot.js
@@ -3,11 +3,39 @@ const fs = require("fs");
 
 const client = new Discord.Client();
 
+let config = JSON.parse(fs.readFileSync("./config.json").toString("utf-8"));
+
 //Event listener to trigger when bot starts
 client.on("ready", () => {
     console.log("Connected and initialized.");
 });
 
 client.on("message", (message) => {
+    if (!message.guild) return;
+    let text = message.content;
+    if (text === "join") {
+        if (message.member.voiceChannel) {
+            message.member.voiceChannel.join().then((connection) => {
 
+            }).catch(console.log);
+        } else {
+            message.channel.send("Channel Not Found.").then();
+        }
+    }
+    if (text === "leave") {
+        if (message.member.voiceChannel) {
+            message.member.voiceChannel.leave();
+        } else {
+            message.channel.send("Channel Not Found").then();
+        }
+    }
+    if (text.substring(0, "play".length) === "play") {
+        let file = "./sounds/" + text.substring("play".length);
+        if (fs.existsSync(file)) {
+
+        }
+
+    }
 });
+
+client.login(config.key).then().catch(console.log);
