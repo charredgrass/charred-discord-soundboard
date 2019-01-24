f3e846fc5f6534a5d899b81f787a08461cb1fb53
Maxwell Chow <maxchowla@gmail.com>
2019-01-24 01:32:34 -0800

    bot commands

--- bot.js
+++ bot.js
@@ -10,29 +10,50 @@ client.on("ready", () => {
     console.log("Connected and initialized.");
 });
 
+let connection, dispatcher;
+
 client.on("message", (message) => {
     if (!message.guild) return;
     let text = message.content;
     if (text === "join") {
-        if (message.member.voiceChannel) {
-            message.member.voiceChannel.join().then((connection) => {
-
+        if (message.member.voiceChannel && !connection) {
+            message.member.voiceChannel.join().then((c) => {
+                connection = c;
             }).catch(console.log);
         } else {
-            message.channel.send("Channel Not Found.").then();
+            if (connection) {
+                message.channel.send("Connection Occupied.").then();
+            } else {
+                message.channel.send("Channel Not Found.").then();
+            }
         }
     }
     if (text === "leave") {
-        if (message.member.voiceChannel) {
-            message.member.voiceChannel.leave();
+        if (connection) {
+            connection.disconnect();
+            connection = null;
+            dispatcher = null;
         } else {
             message.channel.send("Channel Not Found").then();
         }
     }
-    if (text.substring(0, "play".length) === "play") {
-        let file = "./sounds/" + text.substring("play".length);
+    if (text.substring(0, "play ".length) === "play ") {
+        let file = "./sounds/" + text.substring("play ".length);
+        if (!connection) {
+            message.channel.send("No Current Connection, I need to be in a voice channel.").then();
+            return;
+        }
         if (fs.existsSync(file)) {
-
+            if (dispatcher) {
+                message.channel.send("Something is playing already.").then();
+            } else {
+                dispatcher = connection.playFile(file);
+                dispatcher.on("end", () => {
+                    dispatcher = null;
+                });
+            }
+        } else {
+            message.channel.send("File Not Found.").then();
         }
 
     }
